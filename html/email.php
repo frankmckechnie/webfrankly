370b4f9a723f00aa83accd93376005bc016414e0
Frank <frankmckechnie@gmail.com>
2019-09-01 17:47:39 +0100

    added: servce workers

--- html/email.php
+++ html/email.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
     $name = $_POST['name'];
@@ -42,50 +41,5 @@
 	echo '<p>You answered the anti-spam question incorrectly!</p>';
 
     }
-=======
-<?php
-
-    $name = $_POST['name'];
-
-	$surname = $_POST['surname'];
-
-    $email = $_POST['email'];
-
-	$email = $_POST['email'];
-
-    $message = $_POST['message'];
-
-    $from = 'From: TangledDemo'; 
-
-    $to = 'contact@tangledindesign.com'; 
-
-    $subject = 'Hello';
-
-    $human = $_POST['human'];
-
-			
-
-    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-
-				
-
-    if ($_POST['submit'] && $human == '4') {				 
-
-        if (mail ($to, $subject, $body, $from)) { 
-
-	    echo '<p>Your message has been sent!</p>';
-
-	} else { 
-
-	    echo '<p>Something went wrong, go back and try again!</p>'; 
-
-	} 
-
-    } else if ($_POST['submit'] && $human != '4') {
-
-	echo '<p>You answered the anti-spam question incorrectly!</p>';
-
-    }
->>>>>>> e4f532912c6233cf54ca8ca72a23210d4337287a
 
 ?>
--- html/js/main.js
+++ html/js/main.js
@@ -1,128 +1,126 @@
+/**
+ * Webfrankly js 
+ * 
+ * @author Frank Mckechnie
+ */
 
-function myFunction() {
-    var d = new Date();
-    document.getElementById("ndate").innerHTML = "Copyright C "+d.getFullYear()+", Frank Mckechnie";
-}
-
-
-
-$(document).ready(function(){
-
- $('.submit').hide();
+var webfrankly = (function () {
 
 
-
- mnav = $("#mnav");
-
- image = $("#activate");
- 
- image.click(function(){
-   mnav.slideToggle(500);
- });
- 
-  $('.uiButton').click(function() {
-    $(this).toggleClass('uiButtonActive');
-	toogle =  $(this);
-});
+        $(document).ready(function () {
 
 
-});
+                var d = new Date();
+                document.getElementById("ndate").innerHTML =
+                        "Copyright C " + d.getFullYear() + ", Frank Mckechnie";
+                $(".submit").hide();
 
-$( window ).resize(function() {
+                mnav = $("#mnav");
 
-var winW = $( window ).width();
+                image = $("#activate");
 
-  if ( winW > 751 ){
-  mnav.hide();
-  toogle.removeClass('uiButtonActive');
-  
-  }else{
-  
-  }
-});
+                image.click(function () {
+                        mnav.slideToggle(500);
+                });
 
+                $(".uiButton").click(function () {
+                        $(this).toggleClass("uiButtonActive");
+                        toogle = $(this);
+                });
+        });
 
+        $(window).resize(function () {
+                var winW = $(window).width();
 
+                if (winW > 751) {
+                        mnav.hide();
+                        toogle.removeClass("uiButtonActive");
+                } else {}
+        });
 
-roll = new Array();
-for(var r = 0; r <8; r=r+1){
-	roll[r] = new Image();
-
-}
-roll[0].src = "images/rawfacebook.png"; 
-roll[1].src = "images/facebook.png";
-roll[2].src = "images/rawlink.png"; 
-roll[3].src = "images/link.png";
-roll[4].src = "images/rawgoogle.png"; 
-roll[5].src = "images/gplus.png";
-roll[6].src = "images/rawtwitter.png"; 
-roll[7].src = "images/twitter.png";
-
-function swap(myvar){
-document.getElementById('face').src = roll[myvar].src;
-}
-
-function swapTwo(myvar){
-    document.getElementById('link').src = roll[myvar].src;
-}
-
-function swapThree(myvar){
-    document.getElementById('gplus').src = roll[myvar].src;
-}
-
-function swapFour(myvar){
-    document.getElementById('twit').src = roll[myvar].src;
-}
-
-
-
-
-$(window).scroll(function(){  
-	if ($(this).scrollTop() > 200) {
-	 $('.uparrow').fadeIn();
-	}else {
-	 $('.uparrow').fadeOut();
-	 } 
-});
-
-function formcheck() 
-{
-        if (document.theform.name.value == "") 
-        {
-        alert("Please enter your name.");
-	document.theform.name.focus()
-        return false;
+        roll = new Array();
+        for (var r = 0; r < 8; r = r + 1) {
+                roll[r] = new Image();
         }
-		
-			if (document.theform.email.value.indexOf("@") ==
-	 -1||document.theform.email.value == "") 
-        {
-        alert("Please enter a proper email address.");
-	document.theform.email.focus()
-	document.theform.email.value = ""
-        return false;
-        }
-		
-	if (document.theform.surname.value == "") 
-        {
-        alert("Please enter your surname.");
-	document.theform.surname.focus()
-        return false;
-        }
-		
+        roll[0].src = "images/rawfacebook.png";
+        roll[1].src = "images/facebook.png";
+        roll[2].src = "images/rawlink.png";
+        roll[3].src = "images/link.png";
+        roll[4].src = "images/rawgoogle.png";
+        roll[5].src = "images/gplus.png";
+        roll[6].src = "images/rawtwitter.png";
+        roll[7].src = "images/twitter.png";
 
-			if (document.theform.subject.value == "") 
-        {
-        alert("Please enter the subject.");
-	document.theform.telephone.focus()
-        return false;
+        function swap(myvar) {
+                document.getElementById("face").src = roll[myvar].src;
         }
-					if (document.theform.message.value == "") 
-        {
-        alert("Please enter the message.");
-	document.theform.reason.focus()
-        return false;
-        }				
 
-}
-	
+        function swapTwo(myvar) {
+                document.getElementById("link").src = roll[myvar].src;
+        }
+
+        function swapThree(myvar) {
+                document.getElementById("gplus").src = roll[myvar].src;
+        }
+
+        function swapFour(myvar) {
+                document.getElementById("twit").src = roll[myvar].src;
+        }
+
+        $(window).scroll(function () {
+                if ($(this).scrollTop() > 200) {
+                        $(".uparrow").fadeIn();
+                } else {
+                        $(".uparrow").fadeOut();
+                }
+        });
+
+        function formcheck() {
+                if (document.theform.name.value == "") {
+                        alert("Please enter your name.");
+                        document.theform.name.focus();
+                        return false;
+                }
+
+                if (
+                        document.theform.email.value.indexOf("@") == -1 ||
+                        document.theform.email.value == ""
+                ) {
+                        alert("Please enter a proper email address.");
+                        document.theform.email.focus();
+                        document.theform.email.value = "";
+                        return false;
+                }
+
+                if (document.theform.surname.value == "") {
+                        alert("Please enter your surname.");
+                        document.theform.surname.focus();
+                        return false;
+                }
+
+                if (document.theform.subject.value == "") {
+                        alert("Please enter the subject.");
+                        document.theform.telephone.focus();
+                        return false;
+                }
+                if (document.theform.message.value == "") {
+                        alert("Please enter the message.");
+                        document.theform.reason.focus();
+                        return false;
+                }
+        }
+
+        // ServiceWorker is a progressive technology. Ignore unsupported browsers
+        if ('serviceWorker' in navigator) {
+                console.log('CLIENT: service worker registration in progress.');
+                navigator.serviceWorker.register('service-worker.js').then(function () {
+                        console.log('CLIENT: service worker registration complete.');
+                }, function () {
+                        console.log('CLIENT: service worker registration failure.');
+                });
+        } else {
+                console.log('CLIENT: service worker is not supported.');
+        }
+
+
+})();
